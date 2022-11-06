9df9734df17b1ee1ec0cc9528a1ec6be4361119a
Sarthak Narang <sarthaknarang666666@gmail.com>
2022-11-07 05:08:48 +0530

    started backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -28,7 +28,10 @@ const ProductScreen = ({ match }) => {
             <h3>{product.name}</h3>
           </ListGroup.Item>
           <ListGroup.Item>
-            <Rating value={product.rating} text={`${product.reviews}`} />
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
           </ListGroup.Item>
           <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
           <ListGroup.Item>Description: {product.description}</ListGroup.Item>
