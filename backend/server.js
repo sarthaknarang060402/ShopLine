a4094fe0ca2754c7c2487021c531967de88ac2b8
Sarthak Narang <sarthaknarang666666@gmail.com>
2022-11-07 23:46:44 +0530

    error handling

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
@@ -16,6 +16,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 const environment = process.env.NODE_ENV || 'development'
