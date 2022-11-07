f70a36cc1a7224e90927365d6d548695b3f0b95a
Sarthak Narang <sarthaknarang666666@gmail.com>
2022-11-07 09:39:08 +0530

    MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -23,5 +27,7 @@ const PORT = process.env.PORT || 5000
 const environment = process.env.NODE_ENV || 'development'
 app.listen(
   PORT,
-  console.log(`Server running in ${environment} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${environment} mode on port ${PORT}`.yellow.bold
+  )
 )
